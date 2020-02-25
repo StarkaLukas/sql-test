e87c80b6e2d2883541caf1be6e60614c34a45431
StarkaLukas <lstarka141000@gmail.com>
2020-02-25 08:43:48 +0100

    handled exceptions

--- sql.php
+++ sql.php
@@ -1,14 +1,18 @@
 <?php
 
 $_db_host = "localhost";
-$_db_datenbank = "web";
+$_db_database = "web";
 $_db_username = "web";
-$_db_passwort = "web";
+$_db_password = "web";
 
 session_start();
 
-$conn = new mysqli($_db_host, $_db_username, $_db_passwort, $_db_datenbank);
+$conn = new mysqli($_db_host, $_db_username, $_db_password, $_db_database);
 
 if($conn->connect_error){
-    
+    die("Connection failed: " . $conn->connect_error);
 }
+
+$conn->close();
+echo "end of php file";
+?>
